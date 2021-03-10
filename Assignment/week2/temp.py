419af5f43464885f4c5d9ebf144cc9b35dd3d84b
duythinh2708 <79899558+duythinh2708@users.noreply.github.com>
2021-03-10 21:08:11 +0700

    Add files via upload

--- Assignment/week2/temp.py
+++ Assignment/week2/temp.py
@@ -4,7 +4,7 @@ cv2.imshow('image',img)
 #cv2.waitKey(0)
 #cv2.destroyAllWindows()
 img_Gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv2.imshow('Gray Image',img)
+cv2.imshow('Gray Image',img_Gray)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
